4346be65a8b22959774e4c6b364a140ab0c12e51
adamelders <ramor.1990@gmail.com>
2025-09-19 17:59:53 -0400

    refactor: replace files with S1API template, starting from scratch

--- FasterDealers/Core.cs
+++ FasterDealers/Core.cs
@@ -1,61 +1,26 @@
 ﻿using MelonLoader;
-using ScheduleOne.NPCs;
-using UnityEngine;
+using S1API;
+using S1APITemplate.Integrations;
+using S1APITemplate.Utils;
 
-[assembly: MelonInfo(typeof(FasterDealers.Core), "FasterDealers", "1.0.0", "Riccaforte", null)]
-[assembly: MelonGame("TVGS", "Schedule I")]
+[assembly: MelonInfo(typeof(S1APITemplate.Core), Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_AUTHOR)]
+[assembly: MelonGame(Constants.Game.GAME_STUDIO, Constants.Game.GAME_NAME)]
 
-namespace FasterDealers
+namespace S1APITemplate
 {
     public class Core : MelonMod
     {
-        private static MelonPreferences_Category fasterDealersCategory;
-        private static MelonPreferences_Entry<bool> modEnabled;
-        private static MelonPreferences_Entry<float> speedMultiplier;
+        public static Core? Instance { get; private set; }
 
-        public override void OnInitializeMelon() {
-            LoadConfig();
-            LoggerInstance.Msg("Mod initialized and config loaded. Waiting for Main scene to load.");
+        public override void OnInitializeMelon()
+        {
+            Instance = this;
+            HarmonyPatches.SetModInstance(this);
         }
 
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
-            base.OnSceneWasLoaded(buildIndex, sceneName);
-
-            if (!modEnabled.Value) {
-                LoggerInstance.Msg("FasterDealers mod is disabled, skipping.");
-                return;
-            }
-
-            if (sceneName != "Main") {
-                return; // Only run on the Main scene
-            }
-
-            string[] dealerList = ["Benji", "Molly", "Brad", "Jane", "Wei", "Leo"]; // TODO: would be nice to get all dealer "types" or base class
-
-            foreach (string dealerName in dealerList) {
-                SetDealerSpeedMultiplier(dealerName, speedMultiplier.Value);
-            }
-
-            LoggerInstance.Msg("Speed multiplier of " + speedMultiplier.Value.ToString() + " set for all dealer NPCs!");
-        }
-
-        private void SetDealerSpeedMultiplier(string dealerName, float speed) {
-            GameObject dealer = GameObject.Find(dealerName);
-
-            if (dealer != null) {
-                NPCSpeedController component = dealer.GetComponent<NPCSpeedController>();
-                if (component != null) {
-                    component.SpeedMultiplier = speed;
-                }
-            } else {
-                Melon<Core>.Logger.Error($"Dealer {dealerName} not found!");
-            }
-        }
-
-        private void LoadConfig() {
-            fasterDealersCategory = MelonPreferences.CreateCategory("FasterDealers");
-            modEnabled = fasterDealersCategory.CreateEntry<bool>("Enabled", true);
-            speedMultiplier = fasterDealersCategory.CreateEntry<float>("SpeedMultiplier", 3.0f);
+        public override void OnApplicationQuit()
+        {
+            Instance = null;
         }
     }
 }
---
+++ FasterDealers/Integrations/HarmonyPatches.cs
@@ -0,0 +1,23 @@
+﻿#if MONO
+using ScheduleOne;
+#elif IL2CPP
+using Il2CppScheduleOne;
+#endif
+using HarmonyLib;
+
+namespace S1APITemplate.Integrations
+{
+    [HarmonyPatch]
+    public static class HarmonyPatches
+    {
+        private static Core? _modInstance;
+
+        /// <summary>
+        /// Set the mod instance for patch callbacks
+        /// </summary>
+        public static void SetModInstance(Core modInstance)
+        {
+            _modInstance = modInstance;
+        }
+    }
+}
